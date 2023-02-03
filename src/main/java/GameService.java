78353585f523b07a24050e4187f9d81de0d9f51a
moeneal <momo.mneal@gmail.com>
2023-02-03 04:23:07 +0000

    completed mini project 2/2/23

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -27,6 +27,8 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+
+        boolean correctGuess = guessNumber == secretNumber;
+        return correctGuess;
     }
 }
